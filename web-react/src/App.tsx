862277b5c19ef259982ce8522b5fa107a5fe974c
Dante4rt <rxmxdhxni@gmail.com>
2025-06-18 00:20:12 +0700

    feat: add LB

--- web-react/src/App.tsx
+++ web-react/src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Leaderboard from "./pages/Leaderboard";
 // import Leaderboard from "./pages/Leaderboard";
 // import Profile from "./pages/Profile";
 // import Explorer from "./pages/Explorer";
@@ -20,7 +21,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* <Route path="/leaderboard" element={<Leaderboard />} /> */}
+          <Route path="/leaderboard" element={<Leaderboard />} />
           {/* <Route path="/profile" element={<Profile />} /> */}
           {/* <Route path="/explorer" element={<Explorer />} /> */}
           {/* <Route path="/:username" element={<UserProfile />} /> */}
