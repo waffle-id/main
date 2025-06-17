9c8c4535cbe24da0c547ee803688a93cc87ab210
reiyanyan <hariyantoriyan.hr@gmail.com>
2025-06-17 23:18:28 +0700

    Initial commit

---
+++ web/app/routes/home.tsx
@@ -0,0 +1,14 @@
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: "New React Router App" },
+//     { name: "description", content: "Welcome to React Router!" },
+//   ];
+// }
+
+export default function Home() {
+  return (
+    <>
+      <p>hello world</p>
+    </>
+  );
+}
