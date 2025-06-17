e442d4d9ca5cb102e974302f7e91d5e020005035
Dante4rt <rxmxdhxni@gmail.com>
2025-06-18 00:21:46 +0700

    feat: add Profile

--- web-react/src/App.tsx
+++ web-react/src/App.tsx
@@ -5,8 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Leaderboard from "./pages/Leaderboard";
-// import Leaderboard from "./pages/Leaderboard";
-// import Profile from "./pages/Profile";
+import Profile from "./pages/Profile";
 // import Explorer from "./pages/Explorer";
 // import UserProfile from "./pages/UserProfile";
 // import NotFound from "./pages/NotFound";
@@ -22,7 +21,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
-          {/* <Route path="/profile" element={<Profile />} /> */}
+          <Route path="/profile" element={<Profile />} />
           {/* <Route path="/explorer" element={<Explorer />} /> */}
           {/* <Route path="/:username" element={<UserProfile />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
