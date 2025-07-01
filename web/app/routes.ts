2c24c0f031703ab764a5a7a15c598d9670309e30
reiyanyan <hariyantoriyan.hr@gmail.com>
2025-07-02 00:28:44 +0700

    feat: badges

--- web/app/routes.ts
+++ web/app/routes.ts
@@ -8,6 +8,7 @@ export default [
     // route("/hover", "routes/profile/hover.tsx"),
     // route("/hover2", "routes/profile/hover-new.tsx"),
     route("/categories", "routes/categories/index.tsx"),
+    route("/badges", "routes/badges.tsx"),
     route("/leaderboard", "routes/leaderboard/index.tsx"),
   ]),
 ] satisfies RouteConfig;
---
+++ web/app/routes/badges.tsx
@@ -0,0 +1,22 @@
+export default function BadgesPage() {
+  return (
+    <div className="my-44 px-[20px] lg:px-[50px]">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-2 text-center">
+          <p className="text-gray-dark">Earn recognition real community feedback</p>
+          <p className="text-black text-5xl">Badges</p>
+        </div>
+        <div className="grid grid-cols-3 text-black gap-y-12">
+          {new Array(5).fill("").map((val, idx) => (
+            <div key={idx} className="flex flex-col gap-2 h-full mx-2">
+              <p className="font-alt text-2xl">-{idx + 1}</p>
+              <img src="https://placehold.co/10" className="aspect-[4/5] object-cover" />
+              <p className="text-3xl">Categories {idx}</p>
+              <p className="text-gray-dark">Earn recognition real community feedback</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
