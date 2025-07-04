9f463e01b7ee231c2848eb6cbcd7ae97d7bd9740
Dante4rt <rxmxdhxni@gmail.com>
2025-07-05 00:02:54 +0700

    fix: broken pkg

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,9 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  build: {
-    rollupOptions: {
-      external: ["@radix-ui/react-tooltip"],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["@radix-ui/react-tooltip"],
+  //   },
+  // },
 });
